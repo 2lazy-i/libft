34e37510bab999802fe581645ef546090080ce99
Mykhailo Apatenko <2lazy@M.local>
2026-04-07 16:20:02 +0200

    new split and start of the new functions

--- ft_split.c
+++ ft_split.c
@@ -3,113 +3,106 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mapatenk <mapatenk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: 2lazy <2lazy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:17:53 by mapatenk          #+#    #+#             */
-/*   Updated: 2024/10/28 13:58:55 by mapatenk         ###   ########.fr       */
+/*   Updated: 2026/04/07 16:14:44 by 2lazy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	word_count(const char *str, char c);
-static void	init_vars(size_t *i, size_t *index, char ***res, size_t c);
-static void	*mem_free(char **arr);
-static char	*fill_word(const char *s, size_t start, char c);
-
-char	**ft_split(const char *s, char c)
+size_t	count_words(const char *s, char c)
 {
-	char	**res;
-	size_t	i;
-	size_t	index;
-	size_t	start;
-
-	init_vars(&i, &index, &res, word_count(s, c) + 1);
-	if (!res)
-		return (NULL);
-	while (s[i])
-	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != '\0')
-		{
-			start = i;
-			while (s[i] && s[i] != c)
-				i++;
-			res[index] = fill_word(s, start, c);
-			if (!res[index++])
-				return (mem_free(res));
-		}
-	}
-	res[index] = NULL;
-	return (res);
-}
-
-static void	init_vars(size_t *i, size_t *index, char ***res, size_t c)
-{
-	*i = 0;
-	*index = 0;
-	*res = malloc(c * sizeof(char *));
-}
-
-static int	word_count(const char *str, char c)
-{
-	int		count;
-	size_t	i;
+	int i;
+	size_t count;
 
 	count = 0;
 	i = 0;
-	while (str[i])
+	while (s[i])
 	{
-		while (str[i] == c)
+		while(s[i] && s[i] == c)
 			i++;
-		if (str[i] && (i == 0 || str[i - 1] == c))
+		if (s[i] && s[i] != c)
+		{
 			count++;
-		while (str[i] && str[i] != c)
-			i++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
 	}
 	return (count);
 }
 
-static void	*mem_free(char **arr)
+char	*feel_word(const char *s, char c, int start)
 {
-	int	i;
-
-	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
-		i++;
-	}
-	free(arr);
-	return (NULL);
-}
-
-static char	*fill_word(const char *s, size_t start, char c)
-{
-	size_t	len;
+	int		i;
+	int		len;
 	char	*word;
-	size_t	i;
-
-	i = 0;
+	
+	i = start;
 	len = 0;
-	while (s[start + len] && s[start + len] != c)
+	while (s[i] && s[i] != c)
+	{
+		i++;
 		len++;
-	word = malloc(len + 1);
+	}
+	word = malloc((len + 1) * sizeof(char));
 	if (!word)
 		return (NULL);
-	while (i < len)
+	i = 0;
+	while(i < len)
 	{
-		word[i] = s[start + i];
+		word[i] = s[start];
 		i++;
+		start++;
 	}
-	word[len] = '\0';
+	word[i] = '\0';
 	return (word);
+}
+
+void	*mem_free(char **arr, int index)
+{
+	while (index > 0)
+	{
+		index--;
+		free(arr[index]);
+	}
+	free(arr);
+	return(NULL);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int i;
+	int j;
+	char **split;
+
+	split = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!s || !split)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while(s[i])
+	{
+		while(s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			split[j] = feel_word(s, c, i);
+			if (!split[j])
+				return (mem_free(split, j));
+			j++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	split[j] = NULL;
+	return (split);
 }
 
 // int main(void)
 // {
-//     char    str[] = " my final   function  is ready";
+//     char    str[] = " Can you split this  thingNow";
 //     char    ch = ' ';
 //     char    **res;
 //     int     i;
